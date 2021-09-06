541f1aeec36c74283f5cb251f134831a2a1f70d6
anonymousomeone <81277438+anonymousomeone@users.noreply.github.com>
2021-09-05 19:00:51 -0700

    stuff
    extended image range + less console filling errors (hopefully)

--- index.js
+++ index.js
@@ -2,24 +2,46 @@ const got = require('got');
 const jsdom = require("jsdom");
 const download = require('image-downloader')
 const readline = require('readline');
+const URL = require('url').URL;
 const { JSDOM } = jsdom;
 
 const options = { headers: { 'User-Agent': 'Mozilla/4.0' } }
 
 let sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
+const stringIsAValidUrl = (s) => {
+  try {
+    new URL(s);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+function code() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  var random = Math.floor(Math.random() * 2)
+  if (random == 0) {
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+  }
+  return Math.floor(Math.random() * 10)
+}
+
 function rand() {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    var str = alphabet[Math.floor(Math.random() * alphabet.length)] + alphabet[Math.floor(Math.random() * alphabet.length)]
-    return str + Math.floor(Math.random() * 10000);
+  var thing = ''
+  for (var a = 0; a < 6; a++) {
+    thing += code()
+  }
+  return thing
 }
 
 function downloadImage(url, filepath) {
+  if (!stringIsAValidUrl(url)) {return console.log('bad url')}
   try{
     return download.image({
        url,
@@ -40,11 +62,10 @@ async function scrape(reps){
       var image = document.getElementById('screenshot-image')
       var src = image.src;
       downloadImage(src, './scraped')
-      console.log('scraped ' + src)
+      console.log(((i + 1) + '/' + reps) + ' scraped, ' + src + ' (' + url + ')')
     }).catch(() => {
       console.log('something went wrong');
     });
-    console.log((i + 1) + '/' + reps)
     var wait = 4000 + Math.floor(Math.random() * 1000) // dont edit too much or cloudflare will block your ip (speaking from experince)
     await sleep(wait)
   }
